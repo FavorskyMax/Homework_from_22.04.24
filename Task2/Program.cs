6c571db30e04a6274945eb70611f258489d583bf
FavorskyMax <maxvain@mail.ru>
2024-05-05 20:40:04 +0300

    Correct task 2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -6,26 +6,26 @@ int X = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Задай координату Y: ");
 int Y = Convert.ToInt32(Console.ReadLine());
-if (X != 0 && Y != 0)
-{
-    if (X > 0 && Y > 0)
-    {
-        Console.WriteLine("1");
-    }
-    else if (X < 0 && Y > 0)
-    {
-        Console.WriteLine("2");
-    }
-    else if (X < 0 && Y < 0)
-    {
-        Console.WriteLine("3");
-    }
-    else if (X > 0 && Y < 0)
-    {
-        Console.WriteLine("4");
-    }
-}
-else
+if (X != 0 && Y != 0)                                     //  if (X > 0 && Y > 0)
+{                                                         //   { 
+    if (X > 0 && Y > 0)                                   //       Console.WriteLine("1");
+    {                                                     //   }                
+        Console.WriteLine("1");                           //  else if (X < 0 && Y > 0)
+    }                                                     //   {
+    else if (X < 0 && Y > 0)                              //       Console.WriteLine("2");  
+    {                                                     //   } 
+        Console.WriteLine("2");                           //  else if (X < 0 && Y < 0) 
+    }                                                     //   { 
+    else if (X < 0 && Y < 0)                              //       Console.WriteLine("3");
+    {                                                     //   }
+        Console.WriteLine("3");                           //  else if (X > 0 && Y < 0)  
+    }                                                     //   {
+    else if (X > 0 && Y < 0)                              //       Console.WriteLine("4");    
+    {                                                     //   } 
+        Console.WriteLine("4");                           //  else
+    }                                                     //   {
+}                                                         //       Console.WriteLine("Не корректный ввод!");
+else                                                      //   }
 {
     Console.WriteLine("Не корректный ввод!");
 }
