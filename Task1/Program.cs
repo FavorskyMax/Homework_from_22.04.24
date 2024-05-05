48f3cbc99edb121a7a23d8afcad38ffeb676aa9e
FavorskyMax <maxvain@mail.ru>
2024-05-05 19:08:20 +0300

    Completed task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+Console.Write("Напиши число: ");
+int a =Convert.ToInt32(Console.ReadLine());
+int firstDigit = 7;
+int secondDigit = 23;
+
+if (a % firstDigit == 0 && a % secondDigit == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
